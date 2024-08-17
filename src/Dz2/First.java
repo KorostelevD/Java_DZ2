ad6335810d0548ecacbc5f5e28a7b014ba3bc290
Dmytro Korostelyov <m0501543116@gmail.com>
2024-08-17 11:49:44 +0300

    NewInit

--- src/Dz2/First.java
+++ src/Dz2/First.java
@@ -5,38 +5,58 @@ public class First {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // Створення об'єкта Scanner для введення даних з консолі
         String yn; // Змінна для зберігання відповіді користувача
+        long newNumber;
+
+        //цикл
         do {
-            System.out.println("Hello! please enter a number in the decimal system: ");
-            int Number = scanner.nextInt();
-            scanner.nextLine();// Очищення буфера перед зчитуванням рядка
+            System.out.println("Hello! please enter First number in 2, 8, 10 or 16 number system: ");
+            String First = scanner.nextLine(); //введеня першого числа
 
-            System.out.println("Hello! Please enter the number system (number from 2 to 16): ");
-            int sysNumber = scanner.nextInt();
-
-            if (sysNumber < 2 || sysNumber > 16) {
-                while (sysNumber < 2 || sysNumber > 16) {
-                    System.out.println("Error!Enter number from 2 to 16");
-                    scanner.nextLine();// Очищення буфера перед зчитуванням рядка
-                    sysNumber = scanner.nextInt();
-                    if (sysNumber > 1 && sysNumber < 17) {
-                        scanner.nextLine();// Очищення буфера перед зчитуванням рядка
+            //перетворення числа в десяткову систему
+            System.out.println("Please enter Number System of First number (2, 8, 10, 16) : ");
+            int systemFirst = scanner.nextInt(); //введення системи числення для першого числа
+            //якщо
+            if (systemFirst != 2 && systemFirst != 8 && systemFirst != 10 && systemFirst != 16) {
+                while (true) {
+                    System.out.println("Error! Enter 2 or 8 or 10 or 16");
+                    systemFirst = scanner.nextInt();
+                    if (systemFirst == 2 || systemFirst == 8 || systemFirst == 10 || systemFirst == 16) {
                         //Перетворення числа в систему числення
-                        String answer = Integer.toString(Number, sysNumber);
-                        System.out.println("Число: " + Number + " у " + sysNumber + " системі: " + answer);
+                        newNumber = Long.parseLong(First,systemFirst);
+                        System.out.println("Number " + First + " in decimal system is: " + newNumber);
+                        System.out.println("_________________________________________________________ ");
+                        break; //вихід з циклу
+                    }
+                }
+            }else {
+                newNumber = Long.parseLong(First,systemFirst);
+                System.out.println("Number " + First + " in decimal system is: " + newNumber);
+                System.out.println("____________________________________________________________________");
+            }
+
+            //переводимо з десяткової системи в НОВУ
+            System.out.println("Please enter the NEW number system (2, 8, 10 or 16) for decimal number: " + newNumber);
+            int sysNew = scanner.nextInt();
+            if (sysNew != 2 && sysNew != 8 && sysNew != 10 && sysNew != 16) {
+                while (true) {
+                    System.out.println("NEW error! Enter 2 or 8 or 10 or 16");
+                    sysNew = scanner.nextInt();
+                    if (sysNew == 2 || sysNew == 8 || sysNew == 10 || sysNew == 16) {
+                       //Перетворення числа в нову систему числення
+                        String answer = Long.toString(newNumber, sysNew);
+                        System.out.println("Decimal number " + newNumber + " in " + sysNew + " system is: " + answer);
+                        break;
                     }
                 }
             } else {
-                scanner.nextLine();// Очищення буфера перед зчитуванням рядка
                 //Перетворення числа в систему числення
-                String answer = Integer.toString(Number, sysNumber);
-                System.out.println("Число: " + Number + " у " + sysNumber + " системі: " + answer);
-
+                String answer = Long.toString(newNumber, sysNew);
+                System.out.println("Decimal number " + newNumber + " in " + sysNew + " system is: " + answer);
             }
 
-
             System.out.println("Exit, Yes/No");
+            scanner.nextLine();
             yn = scanner.nextLine();
-
         } while (!yn.equalsIgnoreCase("Yes")); // Умова виходу з циклу
     }
 }
